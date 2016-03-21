6df31bad22807876d1b7d017e86c30fcd7c83ed1
pconde3 <pjconde@gatech.edu>
2016-03-21 18:10:23 -0400

    Done.

--- javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
+++ javacs-lab02/src/com/flatironschool/javacs/MyArrayList.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.flatironschool.javacs;
 
@@ -17,9 +17,9 @@ import java.util.ListIterator;
 public class MyArrayList<E> implements List<E> {
 	int size;                    // keeps track of the number of elements
 	private E[] array;           // stores the elements
-	
+
 	/**
-	 * 
+	 *
 	 */
 	public MyArrayList() {
 		// You can't instantiate an array of T[], but you can instantiate an
@@ -39,7 +39,7 @@ public class MyArrayList<E> implements List<E> {
 		mal.add(2);
 		mal.add(3);
 		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
-		
+
 		mal.remove(new Integer(2));
 		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
 	}
@@ -51,7 +51,7 @@ public class MyArrayList<E> implements List<E> {
 			E[] bigger = (E[]) new Object[array.length * 2];
 			System.arraycopy(array, 0, bigger, 0, array.length);
 			array = bigger;
-		} 
+		}
 		array[size] = element;
 		size++;
 		return true;
@@ -63,6 +63,11 @@ public class MyArrayList<E> implements List<E> {
 			throw new IndexOutOfBoundsException();
 		}
 		// TODO: fill in the rest of this method
+		add(element);
+		for (int temp = size - 1; temp > index; temp--) {
+			array[temp] = array[temp - 1];
+		}
+		array[index] = element;
 	}
 
 	@Override
@@ -112,13 +117,20 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: fill in this method
-		return 0;
+		int index = 0;
+		while (index < size) {
+			if (equals(array[index], target)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
 	}
 
 	/** Checks whether an element of the array is the target.
-	 * 
+	 *
 	 * Handles the special case that the target is null.
-	 * 
+	 *
 	 * @param target
 	 * @param object
 	 */
@@ -183,7 +195,12 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public E remove(int index) {
 		// TODO: fill in this method.
-		return null;
+		E temp = get(index);
+		for (int x = index; x < size - 1; x ++) {
+			array[x] = array[x + 1];
+		}
+		size--;
+		return temp;
 	}
 
 	@Override
@@ -203,7 +220,9 @@ public class MyArrayList<E> implements List<E> {
 	@Override
 	public E set(int index, E element) {
 		// TODO: fill in this method.
-		return null;
+		E temp = get(index);
+		array[index] = element;
+		return temp;
 	}
 
 	@Override
@@ -227,6 +246,6 @@ public class MyArrayList<E> implements List<E> {
 
 	@Override
 	public <T> T[] toArray(T[] array) {
-		throw new UnsupportedOperationException();		
+		throw new UnsupportedOperationException();
 	}
 }
